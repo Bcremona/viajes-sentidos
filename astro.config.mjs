2bf166fd041de0e423d1624ef91dd68ad4f3e833
Bautista Cremona <bautistacremona@gmail.com>
2026-01-06 03:14:24 -0300

    static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
-
 import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://bcremona.github.io',
   base: '/viajes-sentidos',
+  output: 'static',
 
   vite: {
     plugins: [tailwindcss()]
