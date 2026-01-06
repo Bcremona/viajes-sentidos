d0e81434bea2a1bc6f1a7f56282f09d704dd36f0
Bautista Cremona <bautistacremona@gmail.com>
2026-01-06 03:07:05 -0300

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bcremona.github.io',
   base: '/viajes-sentidos',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
